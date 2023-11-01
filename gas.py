f9480f5ff76f36927eae16318ac23c74c51a0615
bit-business <101217799+bit-business@users.noreply.github.com>
2023-11-01 16:14:03 +0100

    Update gas.py

--- gas.py
+++ gas.py
@@ -3,22 +3,32 @@ import telegram
 import os
 
 def send_telegram_message(bot_token, chat_id, message):
-    bot = telegram.Bot(token=bot_token)
-    bot.sendMessage(chat_id=chat_id, text=message)
+    try:
+        bot = telegram.Bot(token=bot_token)
+        bot.sendMessage(chat_id=chat_id, text=message)
+    except Exception as e:
+        print(f"Error sending message: {e}")
 
 def check_gas_price():
-    response = requests.get('https://explorer-v1.mxc.com/api/v2/stats')
-    data = response.json()
-    return data['gas_prices']['slow']
+    try:
+        response = requests.get('https://explorer-v1.mxc.com/api/v2/stats')
+        data = response.json()
+        return data['gas_prices']['slow']
+    except Exception as e:
+        print(f"Error fetching gas price: {e}")
+        return None
 
 def main():
-    bot_token = os.environ['TELEGRAM_BOT_TOKEN']
-    chat_id = os.environ['TELEGRAM_CHAT_ID']
-    threshold = 44000000
+    try:
+        bot_token = os.environ['TELEGRAM_BOT_TOKEN']
+        chat_id = os.environ['TELEGRAM_CHAT_ID']
+        threshold = 4000000
 
-    slow_gas_price = check_gas_price()
-    if slow_gas_price < threshold:
-        send_telegram_message(bot_token, chat_id, f'Gas Price Alert: {slow_gas_price}')
+        slow_gas_price = check_gas_price()
+        if slow_gas_price and slow_gas_price < threshold:
+            send_telegram_message(bot_token, chat_id, f'MXC SMALL GAS PRICE: {slow_gas_price}')
+    except Exception as e:
+        print(f"Error in main: {e}")
 
 if __name__ == '__main__':
     main()
